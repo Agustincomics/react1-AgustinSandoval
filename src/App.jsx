00aaff94453de3f1c6fab89057dbfbe91d52593e
Agustin Sandoval <94272906+Agustincomics@users.noreply.github.com>
2023-05-26 23:17:23 -0300

    cambios finales

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+const nombre = "Agustin"
+  return (
+    <>
+      <div>
+        <h1>Hola Mundo: {nombre}</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
